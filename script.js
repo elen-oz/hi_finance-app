038b04a398245dcccf9475639d2b992f31c77784
elen-oz <ozerova.lenka@gmail.com>
2024-02-29 13:32:14 +0100

    fix: update js: form handle

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const input = document.querySelector(".subscribe__input");
-const button = document.querySelector(".subscribe__button");
+const subscribeInputFormEl = document.querySelector('#subscribe__input');
+const subscribeButtonFormEl = document.querySelector('#subscribe__button');
 
-button.addEventListener("click", (e) => {
-	e.preventDefault();
-	if (input.value === "") {
-		alert("Please type your email address.");
-	} else if (!input.value.endsWith("@gmail.com")) {
-		alert("We only approve google mails sorrynotsorry :)");
-	} else {
-		alert("Subscription successful!");
-	}
+subscribeButtonFormEl.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (subscribeInputFormEl.value.trim() === '') {
+    alert('Please type your email address.');
+  } else if (!subscribeInputFormEl.value.includes('@')) {
+    alert('Incorrect e-mail! Try again.');
+  } else {
+    alert('Subscription successful!');
+  }
 });
