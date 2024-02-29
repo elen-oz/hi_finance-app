4f578afa97fc6263936c035421b18e32af3b2c65
elen-oz <ozerova.lenka@gmail.com>
2024-02-29 23:00:01 +0100

    fix ts error

--- script.ts
+++ script.ts
@@ -3,21 +3,16 @@ const subscribeInputFormEl = document.getElementById(
 ) as HTMLInputElement;
 const subscribeButtonFormEl = document.getElementById('subscribe__button');
 
-if (subscribeButtonFormEl) {
-  subscribeButtonFormEl.addEventListener('click', (e) => {
-    e.preventDefault();
-    if (subscribeInputFormEl && subscribeInputFormEl.value.trim() === '') {
-      alert('Please type your email address.');
-    } else if (
-      subscribeInputFormEl &&
-      !subscribeInputFormEl.value.includes('@')
-    ) {
-      alert('Incorrect e-mail! Try again.');
-    } else {
-      alert('Subscription successful!');
-    }
-  });
-}
+subscribeButtonFormEl?.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (subscribeInputFormEl.value.trim() === '') {
+    alert('Please type your email address.');
+  } else if (!subscribeInputFormEl.value.includes('@')) {
+    alert('Incorrect e-mail! Try again.');
+  } else {
+    alert('Subscription successful!');
+  }
+});
 
 const announcementEl = document.getElementById('announcement');
 const announcementCloseBtnEl = document.getElementById(
